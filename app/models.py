63773dedc40954eeef0b11538e2a5d4f586b6cfb
To-Take <s2322039@stu.musashino-u.ac.jp>
2024-11-05 20:49:15 +0900

    コメントモデルの追加

--- app/models.py
+++ app/models.py
@@ -1,8 +1,19 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
-    def __init__(self, id, username, password, role):
+    def __init__(self, id, username, password, role, full_name=None, student_number=None, seminar=None):
         self.id = id
         self.username = username
         self.password = password
         self.role = role
+        self.full_name = full_name
+        self.student_number = student_number
+        self.seminar = seminar
+
+class Comment:
+    def __init__(self, id, portfolio_id, user_id, content, timestamp):
+        self.id = id
+        self.portfolio_id = portfolio_id
+        self.user_id = user_id
+        self.content = content
+        self.timestamp = timestamp
