78e5c52aa666fad2844fedc651a894382b0b49f6
To-Take <s2322039@stu.musashino-u.ac.jp>
2024-11-06 20:19:41 +0900

    進捗用のモデルを追加

--- app/models.py
+++ app/models.py
@@ -17,3 +17,11 @@ class Comment:
         self.user_id = user_id
         self.content = content
         self.timestamp = timestamp
+
+class ProgressUpdate:
+    def __init__(self, id, portfolio_id, progress_number, content, timestamp):
+        self.id = id
+        self.portfolio_id = portfolio_id
+        self.progress_number = progress_number
+        self.content = content
+        self.timestamp = timestamp
